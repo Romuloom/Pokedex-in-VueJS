7da70d753c9b453418a4a7b3b35d4bbd10412e56
Romuloom <romulomesquita.06@gmail.com>
2022-10-24 12:14:53 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+
+  <div class="column is-half is-offset-one-quarter">
+    <img src="./assets/dlf.pt-poliwrath-png-454758.png"/>
+    <hr>
+    <h4 class="is-size-4">1º Geração</h4>
+    <input class="input is-rounded" type="text" name="" id="" placeholder="Buscar pokemon" v-model="busca">
+    <button class="button is-fullwidth is-success" id="buscaBtn" @click="buscar">Buscar</button>
+    <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"></Pokemon>
+     </div>
+  </div>
+
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      pokemons:[],
+      filteredPokemons:[],
+      busca:''
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
+      console.log('Pegou a lista de pokemons')
+      this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results
+    })
+  },
+  components:{
+    Pokemon
+  },
+  methods: {
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons
+      } else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
+  },
+  computed:{
+    /*
+    resultadoBusca:function(){
+      if(this.busca == '' || this.busca == ' '){
+        return this.pokemons;
+      } else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }*/
+    
   }
 }
 </script>
 
 <style>
-#app {
+#app{
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -web-font-smoothing:antialiased;
+  -moz-osx-font-smoothing:grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#buscaBtn{
+  margin-top: 2%;
 }
 </style>
